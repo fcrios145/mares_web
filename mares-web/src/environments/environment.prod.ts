b30e8a6a396f792805b5cac05ab011f96befa255
Francisco Rios <fcrios145@hotmail.com>
2021-10-18 01:08:16 -0500

    add to prod env

--- mares-web/src/environments/environment.prod.ts
+++ mares-web/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDrsDAZvCuE46fsruH-H-Xt16vQSqh4kos",
+    authDomain: "mares-52614.firebaseapp.com",
+    databaseURL: "https://mares-52614-default-rtdb.firebaseio.com",
+    projectId: "mares-52614",
+    storageBucket: "mares-52614.appspot.com",
+    messagingSenderId: "472223781823",
+    appId: "1:472223781823:web:3970ca033a63ff0a64070a",
+    measurementId: "G-3LVTTJLGRE"
+  }
 };
