6724be2f33fca4a58d470d48d479d8e605a945a3
Francisco Rios <fcrios145@hotmail.com>
2021-10-17 23:45:39 -0500

    added angularfire & firebaseconfig values

--- mares-web/src/environments/environment.ts
+++ mares-web/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDrsDAZvCuE46fsruH-H-Xt16vQSqh4kos",
+    authDomain: "mares-52614.firebaseapp.com",
+    databaseURL: "https://mares-52614-default-rtdb.firebaseio.com",
+    projectId: "mares-52614",
+    storageBucket: "mares-52614.appspot.com",
+    messagingSenderId: "472223781823",
+    appId: "1:472223781823:web:3970ca033a63ff0a64070a",
+    measurementId: "G-3LVTTJLGRE"
+  }
 };
 
 /*
